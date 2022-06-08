7540f30e7f5d09386ec3ddeca5f55da95acb7778
sheik17 <sheikabdulkadar.malukkamali@chainsys.com>
2022-06-08 10:41:11 +0530

    second commit

---
+++ src/com/chainsys/bestPractices/abstractDemo/ScoreBoard.java
@@ -0,0 +1,12 @@
+package com.chainsys.bestPractices.abstractDemo;
+
+public class ScoreBoard 
+{
+	public int TargetScore=215;
+   private ScoreBoard(){
+	   System.out.println("Object created for ScoreBoard");
+   }
+   public static ScoreBoard createObject() {
+	   return new ScoreBoard();
+   }
+}
